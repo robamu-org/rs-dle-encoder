0ac632844be965f203101a774f13abde84d9502c
Robin Mueller <robin.mueller.m@gmail.com>
2021-09-15 00:53:21 +0200

    more lib components

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
-struct DleEncoder {
+pub struct DleEncoder {
     escape_stx_etx: bool,
     escape_cr: bool
+}
+
+pub enum DleError {
+    StreamTooShort,
+    DecodingError
 }
 
 impl Default for DleEncoder {
@@ -13,10 +18,15 @@ impl Default for DleEncoder {
 }
 
 impl DleEncoder {
-    pub fn encode() {
-
+    /// This method encodes a given byte stream with ASCII based DLE encoding
+    pub fn encode(
+        source_stream: &[u8], source_len: usize, dest_stream: &mut[u8],
+        max_dest_len: usize
+    ) -> Result<usize, DleError> {
+        Ok(0)
     }
 
+    /// This method decodes an ASCII DLE encoded byte stream
     pub fn decode() {
 
     }
